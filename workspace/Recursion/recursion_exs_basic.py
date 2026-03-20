853b1079c45297d40e5a0a42f58b4e4e06e4c7c1
AbhiUdnur-SWE <abhishek.udnur2@gmail.com>
2026-03-20 15:28:25 +0000

    recursion  exercise

--- workspace/Recursion/recursion_exs_basic.py
+++ workspace/Recursion/recursion_exs_basic.py
@@ -1,3 +1,6 @@
+from ast import For
+
+
 def sum_digit(n):
     if n == 0:
         return 0
@@ -25,3 +28,23 @@ def to_binary(n):
     if n == 1:
         return str(1)
     return to_binary(n//2) + str(n % 2)
+
+
+def flatten(arr):
+    result = []
+
+    for i in arr:
+        if isinstance(i, list):
+            result += flatten(i)
+        else:
+            result.append(i)
+    
+    return result
+
+def nestedEvenSum(obj, sum=0):
+    for key in obj:
+        if type(obj[key]) is dict:
+            sum += nestedEvenSum(obj[key])
+        elif type(obj[key]) is int and obj[key]%2==0:
+            sum+=obj[key]
+    return sum
--- workspace/main.py
+++ workspace/main.py
@@ -1,4 +1,9 @@
 from Recursion import recursion_exs_basic
-
+def power(base, exponent):
+    if exponent == 1:
+        return base
+    base = base*base
+    return power(base, exponent-1)
 if __name__ == "__main__":
-    print(recursion_exs_basic.to_binary(100))
+
+    print(power(2,3))
